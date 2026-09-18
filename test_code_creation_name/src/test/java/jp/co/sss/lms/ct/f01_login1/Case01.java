d6d07ae5fc937abbed206724b34c0e8a084873c3
y-takeda <y-takeda@ISI-OA25L13>
2026-09-18 13:19:19 +0900

    add:Takeda Case01.Case.02テストコード作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case01.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -36,6 +37,12 @@ public class Case01 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
-	}
+		// コンテキストパス /lms を含めたURLを指定
+		String url = "http://localhost:" + 8080 + "/lms";
+		webDriver.get(url);
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {
+		}, "Case01_test01");
 
+	}
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,8 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 /**
  * 結合テスト ログイン機能①
@@ -36,6 +39,15 @@ public class Case02 {
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
 		// TODO ここに追加
+		// コンテキストパス /lms を含めたURLを指定
+		String url = "http://localhost:" + 8080 + "/lms";
+		webDriver.get(url);
+		//タイトルの検証
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		//エビデンス取得
+		getEvidence(new Object() {
+		}, "Case01_test02");
+
 	}
 
 	@Test
@@ -43,6 +55,26 @@ public class Case02 {
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
 	void test02() {
 		// TODO ここに追加
+		String url = "http://localhost:" + 8080 + "/lms";
+		webDriver.get(url);
+		// 存在しないユーザーのログインIDを入力
+		WebElement loginIdElement = webDriver.findElement(By.id("loginId"));
+		loginIdElement.clear();
+		loginIdElement.sendKeys("StudentAA777");
+		// 2. 存在しないユーザーのパスワードを入力
+		WebElement passwordElement = webDriver.findElement(By.id("password"));
+		passwordElement.clear();
+		passwordElement.sendKeys("StudentAA777");
+		//  ログインボタンをクリック
+		webDriver.findElement(By.className("btn-primary")).click();
+		// エラー要素が表示されるまで待つ処理を追加
+		visibilityTimeout(By.cssSelector(".help-inline.error"), 5);
+		WebElement errorElement = webDriver.findElement(By.className("error"));
+		assertEquals("* ログインに失敗しました。", errorElement.getText());
+		//エビデンス（画面スクリーンショット）を取得
+		getEvidence(new Object() {
+		}, "Case01_test02_errorMsg");
+
 	}
 
 }
